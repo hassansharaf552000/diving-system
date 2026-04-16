4e2b48a7235ef8d3e7a976771e403d1e85468e65
habdelhady <habdelhady@2p.com.sa>
2026-04-16 06:44:33 +0200

    fix: update API URL in environment files for production and development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://diveup-api-production-71e6.up.railway.app'
+  apiUrl: 'https://diveup-api-production-e58a.up.railway.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://diveup-api-production-71e6.up.railway.app'
+  apiUrl: 'https://diveup-api-production-e58a.up.railway.app'
 };
